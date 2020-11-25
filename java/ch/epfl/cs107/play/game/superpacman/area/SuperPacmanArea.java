a1a195af3115556148ed3f1a2339935968c520b5
Maud <maud.dennebouy@epfl.ch>
2020-11-25 14:15:31 +0100

    début point 1

---
+++ java/ch/epfl/cs107/play/game/superpacman/area/SuperPacmanArea.java
@@ -0,0 +1,15 @@
+package ch.epfl.cs107.play.game.superpacman.area;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+
+public class SuperPacmanArea extends Area {
+    @Override
+    public float getCameraScaleFactor() {
+        return 15.f;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+}
