5967273d88c67007257a65e0d64a300173f7254c
VictorGitPato <69410903+VictorGitPato@users.noreply.github.com>
2020-11-25 19:21:29 +0100

    -2.2 terminé normalement(déplacement et initialisation); -2.1 un peu de réflexion: j'ai mis des commentaires, en gros on crée des acteurs murs au bon endroit -2.3 classe enum pour les animations créées.

--- java/ch/epfl/cs107/play/game/superpacman/area/Level0.java
+++ java/ch/epfl/cs107/play/game/superpacman/area/Level0.java
@@ -1,11 +1,20 @@
 package ch.epfl.cs107.play.game.superpacman.area;
 
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+
 public class Level0 extends SuperPacmanArea{
+    private DiscreteCoordinates PLAYER_SPAWN_POSTION = new DiscreteCoordinates(10,1);
     public String getTitle() {
         return "superpacman/Level0";
+    }
+
+    @Override
+    public DiscreteCoordinates getSpawnPosition() {
+        return PLAYER_SPAWN_POSITION;
     }
 
     protected void createArea() {
 
     }
+
 }
--- java/ch/epfl/cs107/play/game/superpacman/area/Level1.java
+++ java/ch/epfl/cs107/play/game/superpacman/area/Level1.java
@@ -1,8 +1,16 @@
 package ch.epfl.cs107.play.game.superpacman.area;
 
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+
 public class Level1 extends SuperPacmanArea{
+    private DiscreteCoordinates PLAYER_SPAWN_POSTION =new DiscreteCoordinates(15,6);
     public String getTitle() {
         return "superpacman/Level1";
+    }
+
+    @Override
+    public DiscreteCoordinates getSpawnPosition() {
+        return PLAYER_SPAWN_POSITION;
     }
 
     protected void createArea() {
