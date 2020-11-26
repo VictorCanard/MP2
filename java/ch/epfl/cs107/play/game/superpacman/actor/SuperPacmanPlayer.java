c6b49f02583818ed806c442c79307bdc0491699a
Maud <maud.dennebouy@epfl.ch>
2020-11-26 16:55:51 +0100

    avancement point 3

--- java/ch/epfl/cs107/play/game/superpacman/actor/SuperPacmanPlayer.java
+++ java/ch/epfl/cs107/play/game/superpacman/actor/SuperPacmanPlayer.java
@@ -1,4 +1,20 @@
 package ch.epfl.cs107.play.game.superpacman.actor;
 
-public class SuperPacmanPlayer {
+import ch.epfl.cs107.play.game.areagame.actor.Interactor;
+import ch.epfl.cs107.play.game.areagame.actor.MovableAreaEntity;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.game.rpg.actor.Door;
+import ch.epfl.cs107.play.game.rpg.actor.Player;
+import ch.epfl.cs107.play.game.superpacman.handler.SuperPacmanInteractionVisitor;
+
+public class SuperPacmanPlayer{
+    public class SuperPacmanPlayerHandler implements SuperPacmanInteractionVisitor {
+        @Override
+        public void interactWith(Door door) {
+
+        }
+    }
+
+    public void acceptInteraction(AreaInteractionVisitor v) {
+        ((SuperPacmanInteractionVisitor)v).interactWith(this); }
 }
