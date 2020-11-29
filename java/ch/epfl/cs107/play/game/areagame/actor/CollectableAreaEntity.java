ea6bdd3bede88b5a23fca243a79c11acb2a9c15b
Maud <maud.dennebouy@epfl.ch>
2020-11-29 12:04:38 +0100

    point 3.3, création CollectableAreaEntity, Cherry, Bonus et Diamond

---
+++ java/ch/epfl/cs107/play/game/areagame/actor/CollectableAreaEntity.java
@@ -0,0 +1,19 @@
+package ch.epfl.cs107.play.game.areagame.actor;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+
+public abstract class CollectableAreaEntity extends AreaEntity {
+
+    /**
+     * Default CollectableAreaEntity constructor
+     * @param area (Area): Owner area. Not null
+     * @param position (Coordinate): Initial position of the entity. Not null
+     * @param orientation (Orientation): Initial orientation of the entity. Not null
+     */
+    public CollectableAreaEntity(Area area, Orientation orientation, DiscreteCoordinates position) {
+        super(area, orientation, position);
+    }
+
+    
+}
---
+++ java/ch/epfl/cs107/play/game/superpacman/actor/Cherry.java
@@ -0,0 +1,61 @@
+package ch.epfl.cs107.play.game.superpacman.actor;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.CollectableAreaEntity;
+import ch.epfl.cs107.play.game.areagame.actor.Interactable;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.game.rpg.actor.RPGSprite;
+import ch.epfl.cs107.play.game.superpacman.handler.SuperPacmanInteractionVisitor;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.window.Canvas;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Cherry extends CollectableAreaEntity implements Interactable {
+    Sprite sprite;
+    /**
+     * Default CollectableAreaEntity constructor
+     *
+     * @param area        (Area): Owner area. Not null
+     * @param orientation (Orientation): Initial orientation of the entity. Not null
+     * @param position    (Coordinate): Initial position of the entity. Not null
+     */
+    public Cherry(Area area, Orientation orientation, DiscreteCoordinates position) {
+        super(area, orientation, position);
+        sprite = new RPGSprite("superpacman/cherry",1,1,this);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        if(sprite != null)
+            sprite.draw(canvas);
+    }
+
+    @Override
+    public List<DiscreteCoordinates> getCurrentCells() {
+        return Collections.singletonList(getCurrentMainCellCoordinates());
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return false;
+    }
+
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+
+    @Override
+    public boolean isViewInteractable() {
+        return false;
+    }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v) {
+        v.interactWith(this);
+    }
+}
